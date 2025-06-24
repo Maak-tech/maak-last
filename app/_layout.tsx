a4f8ac41001164f849fb95e265d58dbc12546cab
ahmed-alstaty <ahmad.alstaty@gmail.com>
2025-06-24 18:49:47 +0300

    Fix Navigation Container Error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,18 @@
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native'
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
   useFrameworkReady();
 
   return (
-    <NavigationContainer independent={true}>
+    <>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" />
         <Stack.Screen name="+not-found" options={{ title: 'Not Found' }} />
       </Stack>
       <StatusBar style="auto" />
-    </NavigationContainer>
+    </>
   );
 }
