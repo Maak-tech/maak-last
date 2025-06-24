62acbdf26edcbf4fead84c7f611da6bff7bb0933
ahmed-alstaty <ahmad.alstaty@gmail.com>
2025-06-24 19:30:37 +0300

    Fix React import error in _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, StyleSheet, Platform } from 'react-native';
