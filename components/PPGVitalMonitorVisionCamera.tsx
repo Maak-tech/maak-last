629b968453f7b3176e7e44b3dfb704b11d2db584
Maak-tech <nour@maaktech.net>
2026-01-16 23:47:46 -0500

    ppg updates

--- components/PPGVitalMonitorVisionCamera.tsx
+++ components/PPGVitalMonitorVisionCamera.tsx
@@ -1203,6 +1203,7 @@ export default function PPGVitalMonitorVisionCamera({
     markFrameProcessorCalledOnJS,
     processPPGFrameDataOnJS,
     resetFrameFailureCounterOnJS,
+    setFrameMetaOnJS,
   ]);
 
   const saveVitalToFirestore = async (
