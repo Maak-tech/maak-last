fed2eb1423f6a305b3c76a500a5a30271f756413
Maak-tech <nour@maaktech.net>
2026-02-10 17:48:16 -0500

    Build number update

--- app.config.js
+++ app.config.js
@@ -86,7 +86,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.maaktech.maak",
-      buildNumber: "38",
+      buildNumber: "39",
       jsEngine: "jsc",
       ...(fs.existsSync("./GoogleService-Info.plist") &&
       isValidPlist("./GoogleService-Info.plist")
